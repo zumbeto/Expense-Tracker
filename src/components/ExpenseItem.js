867514bcc35b6ac93093c0a37fa6eded43be1933
Stoyan Peev <stoyanilievpeev@gmail.com>
2023-09-15 20:47:40 +0100

    feat: Add Expenses, ExpenseItem, and ExpenseDate components with styling
    - Created Expenses.js for listing all expense items.
    - Developed ExpenseItem.js to represent a single expense.
    - Introduced ExpenseDate.js for handling and displaying expense dates.
    - Added modular CSS stylesheets for each component.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem = ({ date, title, amount }) => {
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
